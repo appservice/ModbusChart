f2cfae1fbd6ef04424976e3c80a894b616d03b69
luke <luke@luke-300V5A>
2015-02-11 18:57:40 +0100

    commit2-added htmls

--- src/main/resources/static/js/angular/controllers.js
+++ src/main/resources/static/js/angular/controllers.js
@@ -1,3 +1,102 @@
 /**
  * 
  */
+angular.module('myApp.controllers', [])
+.controller('ChartController',[ '$scope', function($scope) {
+
+			/*
+			 * $scope.someData = [ { "year" : "1920", "value" : -0.307 }, {
+			 * "year" : "1970", "value" : -0.068 }, { "year" : "2000", "value" :
+			 * 0.267 }, { "year" : "2001", "value" : 0.411 }, { "year" : "2004",
+			 * "value" : 0.445 }, { "year" : "2005", "value" : 0.47 } ];
+			 */
+
+			$scope.someData = [ {
+				"column-1" : 1,
+				"column-2" : 3,
+				"date" : "2014-03-01 20:58:00"
+			}, {
+				"column-1" : 2,
+				"column-2" : 1,
+				"date" : "2014-03-01 21:58:01"
+			}, {
+				"column-1" : 3,
+				"column-2" : 2,
+				"date" : "2014-03-01 22:58:02"
+			}, {
+				"column-1" : 1,
+				"column-2" : 3,
+				"date" : "2014-03-01 23:58:00"
+			} ];
+		
+	
+
+		} ])
+.controller('ChartOnlineController', [ '$scope', function($scope) 	{
+		$scope.someData = [ ];
+        
+			
+			
+			for(k=0;k<24;k++){
+			for(i=0;i<60;i++){
+			if(k<10){
+			
+				if(i<10){
+					$scope.someData.push(
+						{"column-1":Math.random()*2+Math.cos(i/12)*15+5,
+						"column-2":Math.random()*2+10-i*0.3,
+						"date":"2014-03-02 0"+k+":0"+i+":00"});
+						}
+					else{		
+			
+					$scope.someData.push(
+						{"column-1":Math.random()*2+Math.cos(i/12)*15+5,
+						"column-2":Math.random()*2+10-i*0.3,
+						"date":"2014-03-02 0"+k+":"+i+":00"});
+						}
+				
+				} else{
+				
+							if(i<10){
+					$scope.someData.push(
+						{"column-1":Math.random()*2+Math.cos(i/12)*15+5,
+						"column-2":Math.random()*2+10-i*0.3,
+						"date":"2014-03-02 "+k+":0"+i+":00"});
+						}
+					else{		
+			
+					$scope.someData.push(
+						{"column-1":Math.random()*2+Math.cos(i/12)*15+5,
+						"column-2":Math.random()*2+10-i*0.3,
+						"date":"2014-03-02 "+k+":"+i+":00"});
+						}
+				
+				
+				
+				}
+				}
+			}
+} ])
+.controller('SettingsController',['$scope', function($scope){
+ $scope.serversList=[
+	{'id':1,
+	 'name':'Server 1',
+	 'ip':'192.168.0.183',
+	 'port':1024,
+	 'description':'something'
+	 },
+	 {'id':2,
+	 'name':'Server 2',
+	 'ip':'192.168.0.200',
+	 'port':1024,
+	 'description':'something'
+	 }
+ ];
+ 
+ $scope.removeServer=function(id){
+	$scope.serversList.splice(id,1);
+	
+	}
+	
+}]);
+
--- src/main/resources/static/js/myApp.js
+++ src/main/resources/static/js/myApp.js
@@ -1,13 +1,38 @@
 /**
  * 
  */
-var myApp = angular.module('myApp', [ 'ui.router','ngResource' ]);
+var myApp = angular.module('myApp', [ 'ui.router','ngResource','myApp.directives',
+                                      'myApp.controllers','myApp.services']);
 myApp.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/');
 
-	$stateProvider.state('home', {
+	$stateProvider
+	.state('home', {
 		url : '/',
-		templateUrl : 'view/home.html'///ModbusChart
+		templateUrl : "view/home.html"///ModbusChart
+	})
+	.state('chart',{
+		url:'/chart',
+		templateUrl:'view/chart.html',
+		controller: 'ChartController'
+		
+	})
+	.state('settings',{
+		url:'/settings',
+		templateUrl:'view/admin/settings.html',
+		controller:'SettingsController'
+	
+	})
+		.state('help',{
+		url:'/help',
+		templateUrl:'view/help.html'
+	
+	})
+	.state('chartOnline',{
+		url:'/chart-online',
+		templateUrl:'view/chart-online.html',
+		controller: 'ChartOnlineController'
+	
 	});
 
 	
